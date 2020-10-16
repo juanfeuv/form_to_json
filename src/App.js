35ad47cb885c6dc0271c4e4468822c5e57d8591c
juanfeuv99 <jusugav@unal.edu.co>
2020-10-15 21:57:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [jsonString, setJsonString] = useState('{}');
+
+  const [state, setState] = useState({});
+
+  const generarJSON = e => {
+    e.preventDefault();
+
+    setJsonString(JSON.stringify(state));
+  };
+
+  const changeState = e => {
+    if (e.target.name === 'age') {
+      setState({ ...state, [e.target.name]: Number(e.target.value) });
+    } else {
+      setState({ ...state, [e.target.name]: e.target.value });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          Ingrese su nombre: &nbsp;
+          <input
+            placeholder="Ingrese su nombre"
+            name="name"
+            type="text"
+            onChange={changeState}
+          />
+        </label>
+        <br/>
+        <label>
+          Ingrese su edad: &nbsp;
+          <input
+            placeholder="Ingrese su edad"
+            name="age"
+            type="number"
+            onChange={changeState}
+          />
+        </label>
+        <br/>
+        <br/>
+        <button onClick={generarJSON}>
+          Generar JSON
+        </button>
+      </form>
+      <br/>
+      <br/>
+      <span>JSON traslator</span>
+      <div>
+        {jsonString}
+      </div>
     </div>
   );
 }
